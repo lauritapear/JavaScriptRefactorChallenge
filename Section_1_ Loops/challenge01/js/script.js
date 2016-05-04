9787895e2c5b8ed9dcce441badcc4be540be7f42
LauraArtunduaga <Laura.Artunduaga@ge.com>
2016-05-03 22:32:13 -0400

    adding function for color generator

--- Section_1_ Loops/challenge01/js/script.js
+++ Section_1_ Loops/challenge01/js/script.js
@@ -4,11 +4,16 @@ var green;
 var blue;
 var rgbColor;
 
+function ColorGenerator()
+{
+  return Math.floor(Math.random() * 256 );
+}
+
 for (var i = 0; i< 10; i++)
 {
-  red = Math.floor(Math.random() * 256 );
-  green = Math.floor(Math.random() * 256 );
-  blue = Math.floor(Math.random() * 256 );
+  red = ColorGenerator();
+  green = ColorGenerator();
+  blue = ColorGenerator();
   rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
   html += '<div style="background-color:' + rgbColor + '"></div>';
 }
